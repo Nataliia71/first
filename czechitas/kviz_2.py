3e79e2afb0c13c6e33bca7194e45fbeb2aed3362
natamiroshnichenko89@gmail.com <natamiroshnichenko89@gmail.com>
2024-10-17 16:43:36 +0200

    123

---
+++ czechitas/kviz_2.py
@@ -0,0 +1,44 @@
+# def greet_user(name):
+#     print(f"Ahoj {name}!")
+
+
+# greet_user("Jirko")
+
+
+# def sum_two_numbers(a):
+#      a *10
+# print(sum_two_numbers(5))
+
+
+# def kontrola_emailu(data): 
+#     vystup = [] 
+#     for radek in data: 
+#         if "@" in radek: 
+#             vystup.append(radek)
+#     return vystup 
+# emaily = ["misa@czechitas.cz", 
+#           "kuba@czechitas.cz", 
+#           "soustruh@czechitas.cz", 
+#           "jirkaczechitas.cz"] 
+# print(kontrola_emailu(emaily))
+
+# def kontrola_emailu(data): 
+#     chyby = 0 
+#     for radek in data: 
+#         if "@" not in radek: 
+#             chyby = chyby + 1 
+#     return chyby 
+# emaily = ["misa@czechitas.cz", 
+#           "kuba@czechitas.cz", 
+#           "soustruh@czechitas.cz", 
+#           "jirkaczechitas.cz"] 
+# print(kontrola_emailu(emaily))
+
+# obedy = {"Jirka": "salát", "Kuba": "nudle", "Míša": "nudle"}
+# print(obedy["Petra"])
+
+
+# pozice = {"Lenka": "testerka", "Lucka": "analytička"} 
+# nova_kolegyne = "Petra" 
+# pozice [nova_kolegyne] = "vývojářka"
+oblibene_jazyky = {"Míša", "Python", "Lenka", "Java", "Petra", "SQL"}
